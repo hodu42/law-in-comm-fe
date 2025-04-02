8ba086c74abc01ad72a504b9a9d17ef2102d41ce
hodu42 <knn330@naver.com>
2025-04-02 21:47:36 +0900

    - React Strict Mode 제거 - 테스트용 라우터 정의

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {MainPage} from "./pages/MainPage";
 import {Test} from "./components/Test";
+import {RegisterTest} from "./pages/RegisterTest";
+import {LawyerRegisterTest} from "./pages/LawyerRegisterTest";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
         <Routes>
             <Route path="/" element={<MainPage/>}/>
             <Route path="/test" element={<Test/>}/>
+            <Route path="/register" element={<RegisterTest/>}/>
+            <Route path="/register-lawyer" element={<LawyerRegisterTest/>}/>
         </Routes>
     </BrowserRouter>
   );
