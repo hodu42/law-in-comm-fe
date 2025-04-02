87499afa071cf65d2b5de156a59235045f42094b
hodu42 <knn330@naver.com>
2025-04-02 21:48:40 +0900

    - strict Mode 제거 - Redux 테스트중

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {Provider} from 'react-redux';
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
-
 import reportWebVitals from './reportWebVitals';
-import {MainPage} from "./pages/MainPage";
-import {Test} from "./components/Test";
 import App from "./App";
+import store from "./store";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>
+  <Provider store={store}>
+      <App/>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
