f0efb2c41b1877dca659d1a8bb8ce4c6a7e08bef
hodu42 <knn330@naver.com>
2025-04-04 20:54:01 +0900

    - QuestionList 타입 정의

--- src/types/question.d.ts
+++ src/types/question.d.ts
@@ -1,8 +1,45 @@
-interface Question {
-    legalSpeciality: string;
-    title: string;
-    content: string;
-    firstOccurrenceDate: string;
-    viewCount: number;
-    url: string;
+import {Sort} from "./page";
+import {LegalSpeciality} from "./speciality";
+
+export interface QuestionList {
+    "totalElements": number,
+    "totalPages": number,
+    "first": boolean,
+    "last": boolean,
+    "size": number,
+    "content": Question[],
+    "number": number,
+    "sort": Sort[],
+    "numberOfElements": number,
+    "pageable": {
+        "pageNumber": number,
+        "offset": number,
+        "sort": [
+            {
+                "direction": string,
+                "nullHandling": string,
+                "ascending": boolean,
+                "property": string,
+                "ignoreCase": boolean
+            }
+        ],
+        "pageSize": number,
+        "paged": boolean,
+        "unpaged": boolean
+    }
+}
+
+export interface Question {
+    "questionId": number,
+    "title": string,
+    "content": string,
+    "authorName": string,
+    "authorId": string,
+    "createdAt": string,
+    "updatedAt": string,
+    "legalSpeciality": LegalSpeciality,
+    "firstOccurrenceDate": string,
+    "viewCount": number,
+    "reportCount": number,
+    "anonymous": boolean,
 }
