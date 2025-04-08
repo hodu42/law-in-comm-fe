93b8375b40678c5840c535e63d2ec1817eadd5c2
hodu42 <knn330@naver.com>
2025-04-08 17:05:36 +0900

    모든 라우팅 주소의 /api 제거

--- src/api/index.ts
+++ src/api/index.ts
@@ -61,37 +61,37 @@
    // API 메서드들을 여기에 추가할 수 있습니다
    // 예: 질문 목록 가져오기
    export const getQuestions = async (page: number, size: number) => {
-     return api.get<any>('/api/questions', { page, size });
+     return api.get<any>('/questions', { page, size });
    };
 
    // 질문 상세 가져오기
    export const getQuestion = async (id: number) => {
-     return api.get<any>(`/api/questions/${id}`);
+     return api.get<any>(`/questions/${id}`);
    };
 
    // 질문 작성하기
    export const createQuestion = async (data: any) => {
-     return api.post<any>('/api/questions', data);
+     return api.post<any>('/questions', data);
    };
 
    // 로그인
    export const login = async (username: string, password: string) => {
-     return api.post<any>('/api/login', { username, password });
+     return api.post<any>('/login', { username, password });
    };
 
    // 회원가입
    export const register = async (userData: any) => {
-     return api.post<any>('/api/auth/register', userData);
+     return api.post<any>('/auth/register', userData);
    };
 
    // 일반 사용자 회원가입
    export const registerGeneral = async (userData: any) => {
-     return api.post<any>('/api/users/join/general', userData);
+     return api.post<any>('/users/join/general', userData);
    };
 
    // 변호사 회원가입
    export const registerLawyer = async (formData: FormData) => {
-     return api.post<any>('/api/users/join/lawyer', formData, {
+     return api.post<any>('/users/join/lawyer', formData, {
        headers: {
          'Content-Type': 'multipart/form-data'
        }
@@ -100,25 +100,30 @@
 
    // 닉네임 중복 체크
    export const checkNicknameDuplication = async (nickname: string) => {
-     return api.get<any>('/api/users/join/nickname/dupe-check', { nickname });
+     return api.get<any>('/users/join/nickname/dupe-check', { nickname });
    };
 
    // 법률 전문 분야 조회
    export const getLegalSpecialities = async () => {
-     return api.get<any>('/api/users/legal-speciality');
+     return api.get<any>('/users/legal-speciality');
    };
 
    // 변호사 승인 대기 목록 조회
    export const getPendingLawyers = async (page: number, size: number) => {
-     return api.get<any>('/api/users/admin/confirmations/lawyers', { page, size });
+     return api.get<any>('/users/admin/confirmations/lawyers', { page, size });
    };
 
    // 변호사 승인 대기 상세 조회
    export const getPendingLawyerDetail = async (lawyerId: number) => {
-     return api.get<any>(`/api/users/admin/confirmations/lawyers/${lawyerId}`);
+     return api.get<any>(`/users/admin/confirmations/lawyers/${lawyerId}`);
    };
 
    // 질문 신고하기
    export const reportQuestion = async (questionId: number, reason: string) => {
-     return api.post<any>(`/api/reports/questions/${questionId}`, { reason });
+     return api.post<any>(`/reports/questions/${questionId}`, { reason });
+   };
+
+   // 사용자 정보 조회
+   export const getUserInfo = async (userId: number) => {
+     return api.get<any>(`/users/info/${userId}`);
    };
