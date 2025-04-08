49f92080b535cff720b35c9a3894ca66132408a5
hodu42 <knn330@naver.com>
2025-04-08 17:30:02 +0900

    코드 리팩토링

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import './index.css';
 import {Provider} from 'react-redux';
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
-import reportWebVitals from './reportWebVitals';
 import App from "./App";
 import store from "./store";
 
@@ -16,8 +15,3 @@ root.render(
       <App/>
   </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
