2bf5a8191c330fa55edf8b87f89b98147065ca7b
hodu42 <knn330@naver.com>
2025-04-08 17:06:34 +0900

    라우팅 경로 파일로 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {MainPage} from "./pages/MainPage";
-import {Test} from "./components/Test";
-import {RegisterTest} from "./pages/RegisterTest";
-import {LawyerRegisterTest} from "./pages/LawyerRegisterTest";
-import {PendingLawyersPage} from "./pages/PendingLawyersPage";
-import {WriteTest} from "./pages/WriteTest";
-import {QuestionsTest} from "./pages/QuestionsTest";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { routes } from './routes/routes';
 
 function App() {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<MainPage/>}/>
-            <Route path="/test" element={<Test/>}/>
-            <Route path="/register" element={<RegisterTest/>}/>
-            <Route path="/register-lawyer" element={<LawyerRegisterTest/>}/>
-            <Route path="/pending-lawyers" element={<PendingLawyersPage/>}/>
-            <Route path="/write" element={<WriteTest/>}/>
-            <Route path="/questions" element={<QuestionsTest/>}/>
-        </Routes>
+      <Routes>
+        {routes.map((route) => (
+          <Route key={route.path} path={route.path} element={route.element} />
+        ))}
+      </Routes>
     </BrowserRouter>
   );
 }
---
+++ src/routes/routes.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { RouteObject } from 'react-router-dom';
+import { MainPage } from '../pages/MainPage';
+import { Test } from '../pages/LoginTest';
+import { RegisterTest } from '../pages/RegisterTest';
+import { LawyerRegisterTest } from '../pages/LawyerRegisterTest';
+import { PendingLawyersPage } from '../pages/PendingLawyersPage';
+import { WriteTest } from '../pages/WriteTest';
+import { QuestionsTest } from '../pages/QuestionsTest';
+import { TestPage } from '../pages/TestPage';
+
+export const routes: RouteObject[] = [
+  {
+    path: '/',
+    element: <MainPage />,
+  },
+  {
+    path: '/test',
+    element: <Test />,
+  },
+  {
+    path: '/register',
+    element: <RegisterTest />,
+  },
+  {
+    path: '/register-lawyer',
+    element: <LawyerRegisterTest />,
+  },
+  {
+    path: '/pending-lawyers',
+    element: <PendingLawyersPage />,
+  },
+  {
+    path: '/write',
+    element: <WriteTest />,
+  },
+  {
+    path: '/questions',
+    element: <QuestionsTest />,
+  },
+  {
+    path: '/test-page',
+    element: <TestPage />,
+  },
+]; 
