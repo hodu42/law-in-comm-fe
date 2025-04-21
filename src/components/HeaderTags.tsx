48e782eaf76b80acc45091adf50ed2080005cdf9
hodu42 <knn330@naver.com>
2025-04-21 19:43:40 +0900

    리스트 map key value 추가

--- src/components/HeaderTags.tsx
+++ src/components/HeaderTags.tsx
@@ -4,8 +4,8 @@ export const HeaderTags = ():React.JSX.Element => {
     return (
         // 현재 선택된 페이지에 스타일 적용하도록 만들기
         <nav className="hidden absolute mobile:flex items-center w-full h-[72px] gap-[50px]">
-            {headerPaths.map((path) => (
-                <a href={path.link} className="text-[#CECFD3] text-[24px] font-bold py-4 h-full hover:text-black hover:border-b-black hover:border-b-2" aria-current={path.link === window.location.pathname ? 'page' : undefined}>
+            {headerPaths.map((path, idx) => (
+                <a key={idx} href={path.link} className="text-[#CECFD3] text-[24px] font-bold py-4 h-full hover:text-black hover:border-b-black hover:border-b-2" aria-current={path.link === window.location.pathname ? 'page' : undefined}>
                     {path.title}
                 </a>
             ))}
