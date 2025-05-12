e84038366d6fc65a14faa9611bd171151ad1c4e7
hodu42 <hodu42@DESKTOP-55B75E0>
2025-05-13 02:14:33 +0900

    최근 질문 카루셀 스타일 변경

--- src/components/PrevArrowComponent.tsx
+++ src/components/PrevArrowComponent.tsx
@@ -1,16 +1,28 @@
 import React from "react";
 
-export const PrevArrowComponent = (props:any):React.JSX.Element => {
-    const { className, style, onClick } = props;
-    return (
-        <div
-            className={className}
-            style={{...style, display: "block", background:"none"}}
-            onClick={onClick}
-        >
-            <svg xmlns="http://www.w3.org/2000/svg" width="33" height="66" fill="none" viewBox="0 0 33 66">
-                <path fill="#555555" fillRule="evenodd" d="M21 50a2 2 0 0 0 1.414-3.414L11.828 36l10.586-10.586a2 2 0 1 0-2.828-2.828l-12 12a2 2 0 0 0 0 2.828l12 12A1.994 1.994 0 0 0 21 50z" clipRule="evenodd"/>
-            </svg>
-        </div>
-    )
-}
+export const PrevArrowComponent = (props: any): React.JSX.Element => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={`${className} group`}
+      style={{ ...style, display: "block", background: "none" }}
+      onClick={onClick}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="33"
+        height="66"
+        fill="none"
+        viewBox="0 0 33 66"
+      >
+        <path
+          fill="#555555"
+          fillRule="evenodd"
+          d="M21 50a2 2 0 0 0 1.414-3.414L11.828 36l10.586-10.586a2 2 0 1 0-2.828-2.828l-12 12a2 2 0 0 0 0 2.828l12 12A1.994 1.994 0 0 0 21 50z"
+          clipRule="evenodd"
+          className="group-hover:fill-[#9CB395] transition-colors"
+        />
+      </svg>
+    </div>
+  );
+};
--- src/components/RecentQuestions.tsx
+++ src/components/RecentQuestions.tsx
@@ -1,42 +1,58 @@
-import React, {useRef} from "react";
-import Slider, {Settings} from "react-slick";
-import {PrevArrowComponent} from "@/components/PrevArrowComponent";
-import {NextArrowComponent} from "@/components/NextArrowComponent";
-import {RecentQuestionsContent} from "@/components/RecentQuestionsContent";
-import {Question} from "@/types/question";
+import React, { useRef } from "react";
+import Slider, { Settings } from "react-slick";
+import { PrevArrowComponent } from "@/components/PrevArrowComponent";
+import { NextArrowComponent } from "@/components/NextArrowComponent";
+import { RecentQuestionsContent } from "@/components/RecentQuestionsContent";
+import { Question } from "@/types/question";
 
-export const RecentQuestions = ({recentQuestions}:{recentQuestions:Question[]}):React.JSX.Element => {
-    let sliderRef:React.RefObject<Slider> = useRef<Slider>(null);
-    const settings:Settings = {
-        dots: true,
-        lazyLoad: "anticipated",
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        initialSlide: 0,
-        nextArrow: <NextArrowComponent/>,
-        prevArrow: <PrevArrowComponent/>,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        appendDots: (dots: any) => (
-            <div className="hidden w-full absolute min-[960px]:flex items-center justify-center bottom-6">
-                <ul> {dots} </ul>
-            </div>
-        ),
-        dotsClass: 'dots_custom'
-
-    };
-    return (
-        <section className="flex flex-col gap-[28px] max-w-1350px pc:w-full w-[90%] pc:mt-[144px] mt-[0px] py-[37.6px] bg-[#F7F7FA]">
-            <h1 className="w-full pc:text-center text-left text-[20px] pc:text-[26px] text-[#1F2225] font-bold">최근 질문</h1>
-            <div className="recentQuestion-slider-container min-w-[100%]">
-                <Slider ref={sliderRef} {...settings}>
-                    {recentQuestions.map((question) => (
-                        <RecentQuestionsContent key={question.questionId} {...question}/>
-                    ))}
-                </Slider>
-            </div>
-        </section>
-    )
+interface RecentQuestionsProps {
+  recentQuestions: Question[];
 }
+
+export const RecentQuestions = ({
+  recentQuestions,
+}: RecentQuestionsProps): React.JSX.Element => {
+  let sliderRef: React.RefObject<Slider> = useRef<Slider>(null);
+  const settings: Settings = {
+    dots: true,
+    lazyLoad: "anticipated",
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    nextArrow: <NextArrowComponent />,
+    prevArrow: <PrevArrowComponent />,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    appendDots: (dots: any) => (
+      <div className="hidden w-full absolute min-[960px]:flex items-center justify-center bottom-6">
+        <ul> {dots} </ul>
+      </div>
+    ),
+    dotsClass: "dots_custom",
+  };
+
+  return (
+    <section className="flex flex-col gap-[28px] max-w-1350px pc:w-full w-[90%] pc:mt-[144px] mt-[0px] py-[37.6px] bg-[#F7F7FA]">
+      <h1 className="w-full pc:text-center text-left text-[20px] pc:text-[26px] text-[#1F2225] font-bold">
+        최근 질문
+      </h1>
+      <div className="recentQuestion-slider-container min-w-[100%]">
+        {recentQuestions.length === 0 ? (
+          <div className="flex items-center justify-center font-bold text-[#5C6E56] text-[20px] pc:text-[26px] w-[350px] min-h-[250px] pc:w-[848px] pc:h-[396px] bg-white rounded-[10px] mx-auto p-[20px] pc:p-[30px] border-2 border-[#5C6E56] transition-all relative">
+            최근 질문이 없습니다.
+          </div>
+        ) : recentQuestions.length === 1 ? (
+          <RecentQuestionsContent {...recentQuestions[0]} />
+        ) : (
+          <Slider ref={sliderRef} {...settings}>
+            {recentQuestions.map((question) => (
+              <RecentQuestionsContent key={question.questionId} {...question} />
+            ))}
+          </Slider>
+        )}
+      </div>
+    </section>
+  );
+};
