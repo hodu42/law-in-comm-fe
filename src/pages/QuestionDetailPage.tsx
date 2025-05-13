3df3e35171f882d0e62b2154b9ed5ef966a0ced1
hodu42 <hodu42@DESKTOP-55B75E0>
2025-05-14 00:37:59 +0900

    스타일 수정

--- src/pages/QuestionDetailPage.tsx
+++ src/pages/QuestionDetailPage.tsx
@@ -346,7 +346,7 @@ export const QuestionDetailPage = (): React.JSX.Element => {
                         : "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%239CB395'%3E%3Cpath d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z'/%3E%3C/svg%3E"
                     }
                     alt={`프로필 사진`}
-                    className="w-16 h-16 pc:w-20 pc:h-20 rounded-full border-2 border-[#9CB395]"
+                    className="w-16 h-16 pc:w-20 pc:h-20 rounded-full object-cover border-2 border-[#9CB395]"
                   />
                   <div className="flex-grow">
                     <div className="flex justify-between items-center">
