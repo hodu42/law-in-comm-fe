7a03faa3d4f6d947fed8e3369d30f414273d1ce6
hodu42 <knn330@naver.com>
2025-05-24 15:47:55 +0900

    로고 잘못된 부분 수정

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -25,7 +25,19 @@ export const MainHeader = (): React.JSX.Element => {
                 dispatch(searchActions.setKeyword(""));
               }}
             >
-              <Logo />
+              <svg
+                id="animated-scale"
+                className="text-[#9CB395]"
+                width="48"
+                height="49"
+              >
+                <path
+                  fill="currentColor"
+                  fillRule="evenodd"
+                  d="M41.95 3.029a2.25 2.25 0 0 1-1.729 2.67l-13.97 2.994V39.5h7.5a2.25 2.25 0 0 1 0 4.5h-19.5a2.25 2.25 0 0 1 0-4.5h7.5V9.659l-13.03 2.79a2.251 2.251 0 1 1-.942-4.398l31.5-6.75a2.25 2.25 0 0 1 2.67 1.728Zm-7.02 20.175.02.054a4.362 4.362 0 0 0 8.1 0l.021-.054-4.07-8.142-4.072 8.142ZM39 9.5c-1.38 0-2.64.78-3.255 2.01L30.6 21.797a3 3 0 0 0-.102 2.457l.27.678a8.862 8.862 0 0 0 16.458 0l.27-.678a3 3 0 0 0-.102-2.457l-5.142-10.281A3.63 3.63 0 0 0 39 9.5ZM4.93 29.204l.02.054a4.362 4.362 0 0 0 8.1 0l.021-.054-4.07-8.142-4.072 8.142ZM9 15.5c-1.38 0-2.64.78-3.255 2.01L.6 27.797a3 3 0 0 0-.102 2.457l.27.678a8.862 8.862 0 0 0 16.458 0l.27-.678a3 3 0 0 0-.102-2.457l-5.139-10.284A3.63 3.63 0 0 0 9 15.5Z"
+                  clipRule="evenodd"
+                />
+              </svg>
               <p className="hidden pc:block whitespace-nowrap font-NotoSansKR font-bold text-4xl mr-[50px] text-lightGreen">
                 로인컴
               </p>
